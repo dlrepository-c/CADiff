62ca3e257c38a7d5d8cb909781bcc7a76aee12b0
chenhao181 <ChenHao181@mail.ustc.edu.cn>
2026-03-14 16:17:44 +0800

    update: Modify hyperparameters

--- src/scripts.sh
+++ src/scripts.sh
@@ -2,9 +2,9 @@
 python main.py -d movielens -luser 0.2 -lrec 0.4 -lmse 1.5 -temp 0.6  --warmup 160
 
 # KuaiRec
-python main.py -d kuairec -luser 0.1 -lrec 0.1 -lmse 1.2 -warmup 3
+python main.py -d kuairec -luser 0.1 -lrec 1.0 -lmse 1.0 -max_hl 5 -warmup 3
 
 # Yelp
-python main.py -d yelp -luser 0.05 -lrec 0.6 -lmse 1.0 -temp1.4 --warmup 190
+python main.py -d yelp -luser 0.05 -lrec 0.6 -lmse 1.0 -temp1.4 --warmup 160
 # Books
 python main.py -d books -luser 0.1 -lambda_rec_loss 0.4 -lmse 1.5  --warmup 50 -tau 32
